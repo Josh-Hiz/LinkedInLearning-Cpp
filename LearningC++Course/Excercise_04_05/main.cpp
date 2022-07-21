7aec7725c40e17cf86e0452182051423d6dc10ae
Josh-Hiz <jhizgiaev2704@gmail.com>
2022-07-21 17:47:54 -0400

    Course 1

--- LearningC++Course/Excercise_04_05/main.cpp
+++ LearningC++Course/Excercise_04_05/main.cpp
@@ -1,9 +1,103 @@
 #include <iostream>
 #include <vector>
+#include "records.h"
 
 //GPA = Total points / Total credits
 //A = 4.0 points, B = 3.0 points, C = 2.0 points, D = 1.0 points, F = 0.0 points
+//Total points = grade * credit
+
+std::vector<Student> students = {Student(1, "Jhonny Sins"),
+                                 Student(2, "Big man")};
+
+std::vector<Course> courses = {Course(1,"Algebra",5),
+                               Course(2, "Physics", 4),
+                               Course(3,"English", 3),
+                               Course(4,"Economics", 4)};
+
+std::vector<Grade> grades = {Grade(1,1,'B'), Grade(1,2,'A'), Grade(1,3,'C'), //Johnny Sins
+                             Grade(2,1,'A'), Grade(2, 2, 'A'), Grade(2,4,'B')}; //Big man
+
+float GPA = 0.0f;
+int id;
 
 int main() {
+
+    float totalCredits = 0;
+    float totalPoints = 0;
+
+    std::cout << "Enter student ID" << std::endl;
+    std::cin >> id;
+
+    //Calc GPA, we need a loop to calc total points, and a loop to calc total credits
+
+    //If Johnny Sins
+    if(id == students[0].get_id()) {
+
+        int i = 0;
+
+        while (grades[i].get_student_id() == id) {
+            totalCredits += courses[i].get_credits();
+
+            switch(grades[i].get_grade()){
+                case 'A':
+                    totalPoints += (courses[i].get_credits() * 4);
+                    break;
+                case 'B':
+                    totalPoints += (courses[i].get_credits() * 3);
+                    break;
+                case 'C':
+                    totalPoints += (courses[i].get_credits() * 2);
+                    break;
+                case 'D':
+                    totalPoints += (courses[i].get_credits() * 1);
+                    break;
+                case 'F':
+                    totalPoints += (courses[i].get_credits() * 0);
+                    break;
+            }
+            i++;
+        }
+        GPA = float(totalPoints) / totalCredits;
+        std::cout << "Johnny sins total credits are: " << totalCredits << std::endl; //Should be 12
+        std::cout << "Johnny sins total points are: " << totalPoints << std::endl; //Should be
+        std::cout << "Johnny sins GPA is: " << GPA << std::endl; //Should be
+
+    } else if (id == students[1].get_id()){ //If Big man
+
+        int i = 3;
+        int z = 0;
+        while(grades[i].get_course_id() == courses[z].get_id()){
+            totalCredits += (courses[z].get_credits());
+
+            switch(grades[i].get_grade()){
+                case 'A':
+                    totalPoints += (courses[z].get_credits() * 4);
+                    break;
+                case 'B':
+                    totalPoints += (courses[z].get_credits() * 3);
+                    break;
+                case 'C':
+                    totalPoints += (courses[z].get_credits() * 2);
+                    break;
+                case 'D':
+                    totalPoints += (courses[z].get_credits() * 1);
+                    break;
+                case 'F':
+                    totalPoints += (courses[z].get_credits() * 0);
+                    break;
+            }
+
+            i++;
+            z++;
+        }
+        totalCredits += courses[3].get_credits();
+        totalPoints += (courses[3].get_credits() * 3);
+        GPA = (totalPoints) / totalCredits;
+        std::cout << "Big man total credits are: " << totalCredits << std::endl; //Should be 12
+        std::cout << "Big man total points are: " << totalPoints << std::endl; //Should be
+        std::cout << "Big man GPA is: " << GPA << std::endl; //Should be
+    } else { //If neither
+        std::cout << "Invalid ID" << std::endl;
+    }
     return 0;
 }
---
+++ LearningC++Course/Excercise_04_05/main2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include "records.h"
+
+//GPA = Total points / Total credits
+//A = 4.0 points, B = 3.0 points, C = 2.0 points, D = 1.0 points, F = 0.0 points
+//Total points = grade * credit
+
+std::vector<Student> students = {Student(1, "Jhonny Sins"),
+                                 Student(2, "Big man")};
+
+std::vector<Course> courses = {Course(1,"Algebra",5),
+                               Course(2, "Physics", 4),
+                               Course(3,"English", 3),
+                               Course(4,"Economics", 4)};
+
+std::vector<Grade> grades = {Grade(1,1,'B'), Grade(1,2,'A'), Grade(1,3,'C'), //Johnny Sins
+                             Grade(2,1,'A'), Grade(2, 2, 'A'), Grade(2,4,'B')}; //Big man
+
+float GPA = 0.0f;
+int id;
+
+int main() {
+
+    std::cout << "Enter student ID" << std::endl;
+    std::cin >> id;
+
+    float totalCredits, totalPoints;
+
+    //Get the grade multiplier
+    for (Grade& grd: grades) {
+        if (grd.get_student_id() == id) {
+            float gradeNum = 0;
+            switch (grd.get_grade()) {
+                case 'A':
+                    gradeNum = 4;
+                    break;
+                case 'B':
+                    gradeNum = 3;
+                    break;
+                case 'C':
+                    gradeNum = 2;
+                    break;
+                case 'D':
+                    gradeNum = 1;
+                    break;
+                case 'F':
+                    gradeNum = 0;
+                    break;
+            }
+            //Get total credits and points
+            int i = 0;
+            while (i < courses.size() && courses[i].get_id() == grades[i].get_course_id()) {
+
+                totalCredits += courses[i].get_credits();
+                totalPoints += gradeNum * courses[i].get_credits();
+                i++;
+            }
+        }
+    }
+    GPA = (float) totalPoints / (float) totalCredits;
+
+    std::cout << "GPA for " << students[id - 1].get_name() << " is " << GPA << std::endl;
+
+    return 0;
+}
