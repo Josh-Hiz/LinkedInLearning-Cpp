17594d0748ccd11230fb4361f6f4234147a53a12
Josh-Hiz <jhizgiaev2704@gmail.com>
2022-07-22 21:59:45 -0400

    Course 1

--- LearningC++Course/Excercise_06_01/main.cpp
+++ LearningC++Course/Excercise_06_01/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iostream>
+
+//Reading text files
+
+std::ifstream inFile;
+std::string str;
+int number;
+char letter;
 
 int main() {
+
+    inFile.open("people.txt");
+
+    if(inFile.fail()){
+        std::cout << "Invalid file or no file detected" << std::endl;
+    } else {
+        while(!inFile.eof()){
+            getline(inFile, str);
+            std::cout << str << ", ";
+            getline(inFile, str);
+            number = std::stoi(str);
+            std::cout << number << ", ";
+            getline(inFile, str);
+            letter = str[0];
+            std::cout << letter << std::endl;
+        }
+        inFile.close();
+    }
     return 0;
 }
