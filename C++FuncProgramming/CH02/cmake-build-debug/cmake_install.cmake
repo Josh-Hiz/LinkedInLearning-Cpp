88c67ebe4e3a0cbd63231329feb8e7efb7f2466f
Josh-Hiz <jhizgiaev2704@gmail.com>
2022-08-14 23:31:32 -0400

    Refactoring

--- C++FuncProgramming/CH02/cmake-build-debug/cmake_install.cmake
+++ C++FuncProgramming/CH02/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jhiz2704/C++Projects/C++LinkedInProjects/C++FuncProgramming/CH02_01
+# Install script for directory: /Users/jhiz2704/C++Projects/C++LinkedInProjects/C++FuncProgramming/CH02
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jhiz2704/C++Projects/C++LinkedInProjects/C++FuncProgramming/CH02_01/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/jhiz2704/C++Projects/C++LinkedInProjects/C++FuncProgramming/CH02/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
